901777bb63f75000d428bfb36ec1a242f776b2f8
huangcrab <huangcrab@gmail.com>
2019-05-05 12:01:31 -0400

    heroku try 4

--- api/server.js
+++ api/server.js
@@ -53,7 +53,6 @@ applyMiddleware(errorHandlers, router);
 // 9. Create a server from express instance
 const server = http.createServer(router);
 
-router.use("/", express.static("public"));
 // 10. Start server
 const { mongoUri } = require("./utils/constants");
 mongoose.connect(mongoUri, { useNewUrlParser: true }).then(() => {
